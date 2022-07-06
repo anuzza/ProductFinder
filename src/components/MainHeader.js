fe01b09a7d1ba38545e378a9f6a9ed07d889cb9b
anuzza <anuja3580@gmail.com>
2022-07-06 00:56:41 -0500

    used NavLinks

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,15 +1,15 @@
-import classes from 'MainHeader.module.css';
-import { Link } from "react-router-dom";
+import classes from './MainHeader.module.css';
+import { NavLink } from "react-router-dom";
 const MainHeader =()=>{
 
     return <header className={classes.header}>
         <nav>
             <ul>
                 <li>
-                    <Link to ="/welcome">Welcome</Link>
+                    <NavLink  activeClassName={classes.active} to ="/welcome">Welcome</NavLink>
                 </li>
                 <li>
-                    <Link to = '/products'>Products</Link>
+                    <NavLink activeClassName={classes.active} to = '/products'>Products</NavLink>
                 </li>
             </ul>
 
